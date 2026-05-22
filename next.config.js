6507d1d403c339e79b8a534f586f3d86b3a9e338
ashishmeena2055-svg <ashishmeena2055@gmail.com>
2026-05-22 10:18:19 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  /* यदि आपको भविष्य में किसी बाहरी डोमेन से इमेज लोड करनी हो तो यहाँ सेटिंग्स जोड़ सकते हैं */
+};
+
+module.exports = nextConfig;
